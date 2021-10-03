a81abf6900718303784a79b15b689efc368131d6
danazy <danielani660@gmail.com>
2021-10-03 07:41:12 +0100

    recent update

---
+++ src/Component/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { ThemeContext } from "../Context/ThemeContext";
+class Navbar extends React.Component {
+  static contextType = ThemeContext;
+  render() {
+  
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+              console.log(context);
+              const { isDarkTheme, darkTheme, lightTheme } = context;
+              const theme = isDarkTheme ? darkTheme : lightTheme;
+          <nav
+            style={{
+              background: theme.background,
+              color: theme.text,
+              height: "120px",
+            }}
+          >
+            <h2
+              className="ui centered header"
+              style={{ color: "#fff", padding: 5 }}
+            >
+              Oak Academy
+            </h2>
+            <div className="ui three buttons">
+              <button className="ui button"> Overview</button>
+              <button className="ui button"> Contact</button>
+              <button className="ui button"> Support</button>
+            </div>
+          </nav>;
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+
+export default Navbar;
